2cdd7d9b21c85b63bac0bffcb9c52f6673eb92b7
itzelnavarrete <itzelnavarrete851@gmail.com>
2026-08-06 11:33:19 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
 const SUPABASE_URL = 'https://jmcoxpwrqdlhripedljw.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptY294cHdycWRsaHJpcGVkbGp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MzU1OTgsImV4cCI6MjEwMDMxMTU5OH0.D7PG_csGVtMA2MLvO7tVH5H31gdwp8rQ4PEOWaeuzxE';
+const SUPABASE_KEY = 'sb_publishable_IMDcKiVg8AfveIh-YlrpMw_yb70-F8w';
